bc5df502fa7d544b2d2f6b5f350b4a029b53db4b
Athar Akram <129141365+xthxr@users.noreply.github.com>
2025-07-15 19:54:13 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,16 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import UploadForm from './components/UploadForm';
+import GovtSchemes from './components/GovtSchemes';
 
 function App() {
   return (
-    <div className="bg-white dark:bg-gray-950 min-h-screen text-gray-900 dark:text-white transition-colors duration-300">
+    <div className="bg-white dark:bg-gray-950 text-gray-900 dark:text-white min-h-screen transition-colors duration-300">
       <Navbar />
       <Hero />
       <Features />
       <UploadForm />
+      <GovtSchemes />
     </div>
   );
 }
