9f753ef77c0fc15fa551438b9368a94bc427042b
Athar Akram <129141365+xthxr@users.noreply.github.com>
2025-07-17 00:27:06 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,23 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  darkMode: 'class', // 👈 THIS is important
+  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'typing': 'typing 3s steps(30, end) 1',
+        'fade-up': 'fade-up 1s ease-out',
+      },
+      keyframes: {
+        typing: {
+          'from': { width: '0' },
+          'to': { width: '100%' },
+        },
+        'fade-up': {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        }
+      }
+    },
   },
   plugins: [],
-}
+};
