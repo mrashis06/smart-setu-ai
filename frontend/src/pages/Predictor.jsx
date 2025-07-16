a6bb2b4b375e7bc8fc769b29484c241e24ef13b7
Athar Akram <129141365+xthxr@users.noreply.github.com>
2025-07-16 20:27:14 +0530

    Update Predictor.jsx

--- frontend/src/pages/Predictor.jsx
+++ frontend/src/pages/Predictor.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import { GoogleGenerativeAI } from "@google/generative-ai";
-
-const genAI = new GoogleGenerativeAI(import.meta.env.GEMINI_API_KEY);
 
 const Predictor = () => {
   const [formData, setFormData] = useState({
